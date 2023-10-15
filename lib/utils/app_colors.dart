1b3e19364b07662624a13526d93fbad0daf62000
KashifUmar123 <kashifumar349@gmail.com>
2023-10-15 02:26:21 -0700

    colors added

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color whiteColor = Colors.white;
+const Color blackColor = Color(0XFF19220D);
+const Color lightGreyColor = Color(0XFFD2D2D4);
+const Color greyColor = Color(0XFFAAAAAA);
+const Color darkGreenColor = Color(0XFF2E5111);
+const Color greenColor = Color(0XFF6E8840);
+const Color mossGreenColor = Color(0XFF9B9E5E);
+const Color oliveColor = Color(0XFF5A5A42);
